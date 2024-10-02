cbf0542d7a0da20899b492b9b8b733b97ea5569c
Ondřej Hošek <ondra.hosek@gmail.com>
2024-10-02 04:40:11 +0200

    elliptic curves work now

--- icao-mrtd/src/pace/crypt/elliptic.rs
+++ icao-mrtd/src/pace/crypt/elliptic.rs
@@ -328,27 +328,73 @@ impl PrimeWeierstrassCurve {
         self.is_on_curve_internal(&monty, &x_monty, &y_monty)
     }
 
-    fn double_point_internal(&self, monty: &MontyKnowledge, x_monty: &BoxedMontyForm, y_monty: &BoxedMontyForm, z_monty: &BoxedMontyForm) -> MontyProjectivePoint {
-        if bool::from(y_monty.is_zero()) {
-            return MontyProjectivePoint::AtInfinity;
-        }
+    fn double_point_internal(&self, monty: &MontyKnowledge, x: &BoxedMontyForm, y: &BoxedMontyForm, z: &BoxedMontyForm) -> MontyProjectivePoint {
+        // Renes/Costello/Batina 2015 (https://eprint.iacr.org/2015/1060), Algorithm 3
+        let b3 = (&monty.b).add(&monty.b).add(&monty.b);
 
-        // w = a*z*z + 3*x*x
-        let w = (&monty.a).mul(z_monty).mul(z_monty).add(x_monty.mul(x_monty).mul(&monty.three));
-        // s = y*z
-        let s = y_monty.mul(z_monty);
-        // b = x*y*s
-        let b = x_monty.mul(y_monty).mul(&s);
-        // h = w*w - 8*b
-        let h = (&w).mul(&w).sub((&monty.eight).mul(&b));
-        // x1 = 2*h*s
-        let x1 = (&monty.two).mul(&h).mul(&s);
-        let s_squared = (&s).mul(&s);
-        // y1 = w*(4*b - h) - 8*y*y*s*s
-        let y1 = (&w).mul(&(&monty.four).mul(&b).sub(&h)).sub((&monty.eight).mul(y_monty).mul(y_monty).mul(&s_squared));
-        // z1 = 8*s*s*s
-        let z1 = (&monty.eight).mul(&s_squared).mul(&s);
-        MontyProjectivePoint::Concrete { x: x1, y: y1, z: z1 }
+        // 1. t0 ← X · X
+        let mut t0 = x.mul(x);
+        // 2. t1 ← Y · Y
+        let mut t1 = y.mul(y);
+        // 3. t2 ← Z · Z
+        let mut t2 = z.mul(z);
+        // 4. t3 ← X · Y
+        let mut t3 = x.mul(&y);
+        // 5. t3 ← t3 + t3
+        t3 = (&t3).add(&t3);
+        // 6. Z3 ← X · Z
+        let mut z3 = x.mul(z);
+        // 7. Z3 ← Z3 + Z3
+        z3 = (&z3).add(&z3);
+        // 8. X3 ← a · Z3
+        let mut x3 = (&monty.a).mul(&z3);
+        // 9. Y3 ← b3 · t2
+        let mut y3 = (&b3).mul(&t2);
+        // 10. Y3 ← X3 + Y3
+        y3 = (&x3).add(&y3);
+        // 11. X3 ← t1 − Y3
+        x3 = (&t1).sub(&y3);
+        // 12. Y3 ← t1 + Y3
+        y3 = (&t1).add(&y3);
+        // 13. Y3 ← X3 · Y3
+        y3 = (&x3).mul(&y3);
+        // 14. X3 ← t3 · X3
+        x3 = (&t3).mul(&x3);
+        // 15. Z3 ← b3 · Z3
+        z3 = (&b3).mul(&z3);
+        // 16. t2 ← a · t2
+        t2 = (&monty.a).mul(&t2);
+        // 17. t3 ← t0 − t2
+        t3 = (&t0).sub(&t2);
+        // 18. t3 ← a · t3
+        t3 = (&monty.a).mul(&t3);
+        // 19. t3 ← t3 + Z3
+        t3 = (&t3).add(&z3);
+        // 20. Z3 ← t0 + t0
+        z3 = (&t0).add(&t0);
+        // 21. t0 ← Z3 + t0
+        t0 = (&z3).add(&t0);
+        // 22. t0 ← t0 + t2
+        t0 = (&t0).add(&t2);
+        // 23. t0 ← t0 · t3
+        t0 = (&t0).mul(&t3);
+        // 24. Y3 ← Y3 + t0
+        y3 = (&y3).add(&t0);
+        // 25. t2 ← Y · Z
+        t2 = y.mul(z);
+        // 26. t2 ← t2 + t2
+        t2 = (&t2).add(&t2);
+        // 27. t0 ← t2 · t3
+        t0 = (&t2).mul(&t3);
+        // 28. X3 ← X3 − t0
+        x3 = (&x3).sub(&t0);
+        // 29. Z3 ← t2 · t1
+        z3 = (&t2).mul(&t1);
+        // 30. Z3 ← Z3 + Z3
+        z3 = (&z3).add(&z3);
+        // 31. Z3 ← Z3 + Z3
+        z3 = (&z3).add(&z3);
+        MontyProjectivePoint::Concrete { x: x3, y: y3, z: z3 }
     }
 
     pub fn double_point(&self, point: &ConcreteProjectivePoint) -> ProjectivePoint {
@@ -365,52 +411,103 @@ impl PrimeWeierstrassCurve {
     }
 
     fn add_points_internal(&self, monty: &MontyKnowledge, x1: &BoxedMontyForm, y1: &BoxedMontyForm, z1: &BoxedMontyForm, x2: &BoxedMontyForm, y2: &BoxedMontyForm, z2: &BoxedMontyForm) -> MontyProjectivePoint {
-        // u1 = y2*z1
-        let u1 = y2.mul(z1);
-        // u2 = y1*z2
-        let u2= y1.mul(z2);
-        // v1 = x2*z1
-        let v1 = x2.mul(z1);
-        // v2 = x1*z2
-        let v2 = x1.mul(z2);
-        if v1 == v2 {
-            return if u1 != u2 {
-                MontyProjectivePoint::AtInfinity
-            } else {
-                self.double_point_internal(monty, x1, y1, z1)
-            };
-        }
-        // u = u1 - u2
-        let u = (&u1).sub(&u2);
-        // v = v1 - v2
-        let v = (&v1).sub(&v2);
-        // w = z1*z2
-        let w = z1.mul(z2);
-        // a = u*u*w - v*v*v - 2*v*v*v2
-        let v_squared = (&v).mul(&v);
-        let v_cubed = (&v_squared).mul(&v);
-        let v_v_v2 = (&v_squared).mul(&v2);
-        let a = (&u).mul(&u).mul(&v).sub((&v_squared).mul(&v)).sub((&monty.two).mul(&v_v_v2));
-        // x3 = v*a
-        let x3 = (&v).mul(&a);
-        // y3 = u*(v*v*v2 - a) - v*v*v*u2
-        let y3 = (&u).mul(&(&v_v_v2).sub(&a)).sub((&v_cubed).mul(&u2));
-        // z3 = v^3*w
-        let z3 = (&v_cubed).mul(&w);
+        // Renes/Costello/Batina 2015 (https://eprint.iacr.org/2015/1060), Algorithm 1
+        let b3 = (&monty.b).add(&monty.b).add(&monty.b);
+
+        // 1. t0 ← X1 · X2
+        let mut t0 = x1.mul(x2);
+        // 2. t1 ← Y1 · Y2
+        let mut t1 = y1.mul(y2);
+        // 3. t2 ← Z1 · Z2
+        let mut t2 = z1.mul(z2);
+        // 4. t3 ← X1 + Y1
+        let mut t3 = x1.add(y1);
+        // 5. t4 ← X2 + Y2
+        let mut t4 = x2.add(y2);
+        // 6. t3 ← t3 · t4
+        t3 = (&t3).mul(&t4);
+        // 7. t4 ← t0 + t1
+        t4 = (&t0).add(&t1);
+        // 8. t3 ← t3 − t4
+        t3 = (&t3).sub(&t4);
+        // 9. t4 ← X1 + Z1
+        t4 = (&x1).add(&z1);
+        // 10. t5 ← X2 + Z2
+        let mut t5 = (&x2).add(&z2);
+        // 11. t4 ← t4 · t5
+        t4 = (&t4).mul(&t5);
+        // 12. t5 ← t0 + t2
+        t5 = (&t0).add(&t2);
+        // 13. t4 ← t4 − t5
+        t4 = (&t4).sub(&t5);
+        // 14. t5 ← Y1 + Z1
+        t5 = (&y1).add(&z1);
+        // 15. X3 ← Y2 + Z2
+        let mut x3 = (&y2).add(&z2);
+        // 16. t5 ← t5 · X3
+        t5 = (&t5).mul(&x3);
+        // 17. X3 ← t1 + t2
+        x3 = (&t1).add(&t2);
+        // 18. t5 ← t5 − X3
+        t5 = (&t5).sub(&x3);
+        // 19. Z3 ← a · t4
+        let mut z3 = (&monty.a).mul(&t4);
+        // 20. X3 ← b3 · t2
+        x3 = (&b3).mul(&t2);
+        // 21. Z3 ← X3 + Z3
+        z3 = (&x3).add(&z3);
+        // 22. X3 ← t1 − Z3
+        x3 = (&t1).sub(&z3);
+        // 23. Z3 ← t1 + Z3
+        z3 = (&t1).add(&z3);
+        // 24. Y3 ← X3 · Z3
+        let mut y3 = (&x3).mul(&z3);
+        // 25. t1 ← t0 + t0
+        t1 = (&t0).add(&t0);
+        // 26. t1 ← t1 + t0
+        t1 = (&t1).add(&t0);
+        // 27. t2 ← a · t2
+        t2 = (&monty.a).mul(&t2);
+        // 28. t4 ← b3 · t4
+        t4 = (&b3).mul(&t4);
+        // 29. t1 ← t1 + t2
+        t1 = (&t1).add(&t2);
+        // 30. t2 ← t0 − t2
+        t2 = (&t0).sub(&t2);
+        // 31. t2 ← a · t2
+        t2 = (&monty.a).mul(&t2);
+        // 32. t4 ← t4 + t2
+        t4 = (&t4).add(&t2);
+        // 33. t0 ← t1 · t4
+        t0 = (&t1).mul(&t4);
+        // 34. Y3 ← Y3 + t0
+        y3 = (&y3).add(&t0);
+        // 35. t0 ← t5 · t4
+        t0 = (&t5).mul(&t4);
+        // 36. X3 ← t3 · X3
+        x3 = (&t3).mul(&x3);
+        // 37. X3 ← X3 − t0
+        x3 = (&x3).sub(&t0);
+        // 38. t0 ← t3 · t1
+        t0 = (&t3).mul(&t1);
+        // 39. Z3 ← t5 · Z3
+        z3 = (&t5).mul(&z3);
+        // 40. Z3 ← Z3 + t0
+        z3 = (&z3).add(&t0);
         MontyProjectivePoint::Concrete { x: x3, y: y3, z: z3 }
     }
 
     pub fn add_points(&self, lhs: &ConcreteProjectivePoint, rhs: &ConcreteProjectivePoint) -> ProjectivePoint {
         let monty = self.monty_knowledge();
         let (x1, y1, z1) = (
-            BoxedMontyForm::new(lhs.x.clone(), monty.params.clone()),
-            BoxedMontyForm::new(lhs.y.clone(), monty.params.clone()),
-            BoxedMontyForm::new(lhs.z.clone(), monty.params.clone()),
+            BoxedMontyForm::new(lhs.x.widen(monty.params.bits_precision()), monty.params.clone()),
+            BoxedMontyForm::new(lhs.y.widen(monty.params.bits_precision()), monty.params.clone()),
+            BoxedMontyForm::new(lhs.z.widen(monty.params.bits_precision()), monty.params.clone()),
         );
         let (x2, y2, z2) = (
-            BoxedMontyForm::new(rhs.x.clone(), monty.params.clone()),
-            BoxedMontyForm::new(rhs.y.clone(), monty.params.clone()),
-            BoxedMontyForm::new(rhs.z.clone(), monty.params.clone()),
+            BoxedMontyForm::new(rhs.x.widen(monty.params.bits_precision()), monty.params.clone()),
+            BoxedMontyForm::new(rhs.y.widen(monty.params.bits_precision()), monty.params.clone()),
+            BoxedMontyForm::new(rhs.z.widen(monty.params.bits_precision()), monty.params.clone()),
         );
         self.add_points_internal(&monty, &x1, &y1, &z1, &x2, &y2, &z2).retrieve()
     }
@@ -439,7 +536,16 @@ impl PrimeWeierstrassCurve {
         result.retrieve()
     }
 
-    fn multiply_scalar_with_affine(&self, scalar: &BoxedUint, point: &AffinePoint) -> AffinePoint {
+    pub fn add_affine(&self, left: &AffinePoint, right: &AffinePoint) -> AffinePoint {
+        let left_projective = left.clone().into_projective();
+        let right_projective = right.clone().into_projective();
+        self.add_points(&left_projective, &right_projective)
+            .as_concrete().unwrap()
+            .clone()
+            .into_affine(&self.prime).unwrap()
+    }
+
+    pub fn multiply_scalar_with_affine(&self, scalar: &BoxedUint, point: &AffinePoint) -> AffinePoint {
         let point_projective = point.clone().into_projective();
         let multiplied_projective = self.multiply_scalar_with_point(scalar, &point_projective);
         multiplied_projective
@@ -495,14 +601,20 @@ mod tests {
             547EF835 C3DAC4FD 97F8461A 14611DC9
             C2774513 2DED8E54 5C1D54C7 2F046997
         "));
-
+        let generator = AffinePoint::new(generator_x, generator_y);
         let curve = PrimeWeierstrassCurve::new(
-            prime,
-            coefficient_a,
-            coefficient_b,
-            AffinePoint::new(generator_x, generator_y),
+            prime.clone(),
+            coefficient_a.clone(),
+            coefficient_b.clone(),
+            generator.clone(),
         );
 
+        // obtain nonce
+        let nonce = boxed_uint_from_be_slice(&hex!("
+            3F00C4D3 9D153F2B 2A214A07 8D899B22
+        "));
+
+        // perform key agreement for session secret
         let terminal_private = boxed_uint_from_be_slice(&hex!("
             7F4EF07B 9EA82FD7 8AD689B3 8D0BC78C
             F21F249D 953BC46F 4C6E1925 9C010F99
@@ -560,6 +672,81 @@ mod tests {
         assert_eq!(terminal_secret, shared_secret);
         assert_eq!(chip_secret, shared_secret);
 
-        // TODO: mapping
+        // map a new generator:
+        // (nonce * original_generator) + shared_secret
+        let nonced_generator = curve.multiply_scalar_with_affine(&nonce, &generator);
+        let session_generator = curve.add_affine(&nonced_generator, &shared_secret);
+        assert_eq!(
+            session_generator.x,
+            boxed_uint_from_be_slice(&hex!("
+                8CED63C9 1426D4F0 EB1435E7 CB1D74A4
+                6723A0AF 21C89634 F65A9AE8 7A9265E2
+            ")),
+        );
+        assert_eq!(
+            session_generator.y,
+            boxed_uint_from_be_slice(&hex!("
+                8C879506 743F8611 AC33645C 5B985C80
+                B5F09A0B 83407C1B 6A4D857A E76FE522
+            ")),
+        );
+
+        // set up a curve with the same coefficients but this new generator
+        let session_curve = PrimeWeierstrassCurve::new(
+            prime,
+            coefficient_a,
+            coefficient_b,
+            session_generator,
+        );
+
+        let session_terminal_private = boxed_uint_from_be_slice(&hex!("
+            A73FB703 AC1436A1 8E0CFA5A BB3F7BEC
+            7A070E7A 6788486B EE230C4A 22762595
+        "));
+        let session_chip_private = boxed_uint_from_be_slice(&hex!("
+            107CF586 96EF6155 053340FD 633392BA
+            81909DF7 B9706F22 6F32086C 7AFF974A
+        "));
+
+        let session_terminal_public = session_curve.calculate_public_key(&session_terminal_private);
+        let session_chip_public = session_curve.calculate_public_key(&session_chip_private);
+
+        assert_eq!(
+            session_terminal_public.x,
+            boxed_uint_from_be_slice(&hex!("
+                2DB7A64C 0355044E C9DF1905 14C625CB
+                A2CEA487 54887122 F3A5EF0D 5EDD301C
+            ")),
+        );
+        assert_eq!(
+            session_terminal_public.y,
+            boxed_uint_from_be_slice(&hex!("
+                3556F3B3 B186DF10 B857B58F 6A7EB80F
+                20BA5DC7 BE1D43D9 BF850149 FBB36462
+            ")),
+        );
+        assert_eq!(
+            session_chip_public.x,
+            boxed_uint_from_be_slice(&hex!("
+                9E880F84 2905B8B3 181F7AF7 CAA9F0EF
+                B743847F 44A306D2 D28C1D9E C65DF6DB
+            ")),
+        );
+        assert_eq!(
+            session_chip_public.y,
+            boxed_uint_from_be_slice(&hex!("
+                7764B222 77A2EDDC 3C265A9F 018F9CB8
+                52E111B7 68B32690 4B59A019 3776F094
+            ")),
+        );
+
+        let session_terminal_secret = session_curve.diffie_hellman(&session_terminal_private, &session_chip_public);
+        let session_chip_secret = session_curve.diffie_hellman(&session_chip_private, &session_terminal_public);
+        let session_shared_secret = boxed_uint_from_be_slice(&hex!("
+            28768D20 701247DA E81804C9 E780EDE5 
+            82A9996D B4A31502 0B273319 7DB84925
+        "));
+        assert_eq!(session_terminal_secret.x, session_shared_secret);
+        assert_eq!(session_chip_secret.x, session_shared_secret);
     }
 }
